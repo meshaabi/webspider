11eaf158c356ed9a29bda326afccec2be44a83ae
hackfanatic <esh@esh-XPS-L501X>
2011-03-17 11:54:39 +0000

    nothing has changed

--- src/webspider/RunSpider.java
+++ src/webspider/RunSpider.java
@@ -4,7 +4,6 @@
  */
 
 package webspider;
-import webspider.actions.CrawlerActions;
 import webspider.actions.SpiderActions;
 import webspider.gui.CrawlPanel;
 import webspider.gui.IndexerPanel;
@@ -37,8 +36,18 @@ public class RunSpider {
                     Settings.BACK_BUTTON = false;
                     actions.setPanel(new SearchPanel(actions));
                 }
+                gui.run();
+            }else if(args[0].equals("-cli")){
+                Settings.NO_GUI = true;
+                try{
+                    if(args[1].equals("c")){
+                    }else if(args[1].equals("i")){
+                    }else if(args[1].equals("s")){
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            gui.run();
         }
         
     }

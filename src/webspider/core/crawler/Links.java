ea73fc7b2d62b9f4b6be4404f19674fb5e9f644a
Zsolt Bitvai <bitvaizs@gmail.com>
2011-03-18 14:08:10 +0000

    fewfwef

--- src/webspider/core/crawler/Links.java
+++ src/webspider/core/crawler/Links.java
@@ -11,64 +11,73 @@ import java.util.LinkedHashSet;
 
 /**
  * A collection of links and operations on them
+ * 
  * @author Zsolt Bitvai
  */
-public class Links implements Iterable<URL>{
-	
+public class Links implements Iterable<URL> {
+
 	/**
 	 * the urls to hold
 	 */
 	private Collection<URL> urls;
 
-	/**	 * the path to print the urls
-	 * The path to print the urls
+	/**
+	 * * the path to print the urls The path to print the urls
 	 */
 	private String printPath;
 
-	public Links( String printPath){
+	public Links(String printPath) {
 		this.printPath = printPath;
 		this.urls = Collections.synchronizedSet(new LinkedHashSet<URL>());
 	}
+
 	/**
 	 * print the urls
+	 * 
 	 * @throws FileNotFoundException
 	 */
-	public void print() throws FileNotFoundException{
+	public void print() throws FileNotFoundException {
 		File outfile = new File(this.printPath);
-            if (outfile.mkdirs()){
-                    PrintWriter urlWriter = new PrintWriter(outfile);
-                    synchronized (this.urls) {
-                            for (URL url : this.urls) {
-                                    urlWriter.println(url);
-                            }
-                    }
-                    urlWriter.flush();
-                    urlWriter.close();
-            }
-        }
+		PrintWriter urlWriter = new PrintWriter(outfile);
+		synchronized (this.urls) {
+			for (URL url : this.urls) {
+				urlWriter.println(url);
+			}
+		}
+		urlWriter.flush();
+		urlWriter.close();
+
+	}
+
 	/**
 	 * Add a new url to the collection
+	 * 
 	 * @param url
 	 */
 	public void add(URL url) {
-		this.urls.add(url);			
+		this.urls.add(url);
 	}
 
 	/**
 	 * Get the number of links to collection holds
+	 * 
 	 * @return the size of the collection
 	 */
-	public int size(){
+	public int size() {
 		return this.urls.size();
 	}
+
 	/**
 	 * Check that an url is in the collection already
-	 * @param checkUrl the url to check
+	 * 
+	 * @param checkUrl
+	 *            the url to check
 	 * @return is the url contained by the collection?
 	 */
-	public boolean contains(URL checkUrl){
+	public boolean contains(URL checkUrl) {
 		return this.urls.contains(checkUrl);
 	}
+
 	/**
 	 * Iterate over all urls in this collection
 	 */
@@ -76,13 +85,14 @@ public class Links implements Iterable<URL>{
 	public Iterator<URL> iterator() {
 		return this.urls.iterator();
 	}
-	
+
 	/**
 	 * Returns all the links in the collection
+	 * 
 	 * @return the links
 	 */
-	public Collection<URL> getLinks(){
+	public Collection<URL> getLinks() {
 		return this.urls;
 	}
-	
+
 }

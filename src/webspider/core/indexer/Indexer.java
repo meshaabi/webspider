bc174584de7c3c2d595fcc646e9cc84d599abc8d
Kushal Lyndon D'Souza <kushlyn@gmail.com>
2011-03-18 13:49:09 +0000

    Kushal

--- src/webspider/core/indexer/Indexer.java
+++ src/webspider/core/indexer/Indexer.java
@@ -12,23 +12,37 @@ import webspider.actions.SpiderActions;
 import webspider.gui.IndexerPanel;
 
 /**
- * 
+ * This class implements the myIWSearchEngine interface and controls the running
+ * of the indexer.
  * @author Kushal D'Souza
  */
 public class Indexer implements myIWSearchEngine{
 
+    // Variables declared here
+
+    /**
+     * Instance of IndexerImpl.
+     */
     private IndexerImpl indexer;
-    SpiderActions actions;
-    /*
-     * Constructor
-     * @param actions instance of SpiderActions class
+
+    /**
+     *  Instance of SpiderActions class.
+     */
+    private SpiderActions actions;
+
+    
+    /**
+     * Constructor for Indexer.java.
+     * 
+     * @param actions instance of SpiderActions class.
      */
     public Indexer(SpiderActions actions){
         this.actions = actions;
     }
 
-    /*
-     * Calls the start indexing function
+    /**
+     * Calls the startIndexing function.
+     * 
      * @param inputFileName input file name
      * @param outputFileName output file name
      */
@@ -38,13 +52,13 @@ public class Indexer implements myIWSearchEngine{
         this.indexer.startIndexing();
     }
 
-    /*
+    /**
      * Calls the startLoadIndex function which loads the index from a file in
-     * a new thread
-     * @param filename name of file from which index is loaded
+     * a new thread.
+     * 
+     * @param fileName input file from where the index is loaded into memory.
      * @return Map Map containing the index table
      */
-
     public Map loadIndexTable(String fileName)
     {
         this.indexer = new IndexerImpl(actions);
@@ -54,8 +68,9 @@ public class Indexer implements myIWSearchEngine{
     }
 
 
-    /*
-     * Calls the start search function
+    /**
+     * Calls the start search function.
+     * 
      * @param keyword keyword on which search is run
      * @return Set<URL> set of urls containing the keyword
      */
@@ -69,22 +84,30 @@ public class Indexer implements myIWSearchEngine{
         return results;
     }
 
+    /**
+     * Opens the user interface.
+     */
     public void openUserInterface() {
         Settings.BACK_BUTTON = false;
         actions.setPanel(new IndexerPanel(actions));
         actions.openInterface();
     }
 
+    /**
+     * Closes the user iterface.
+     */
     public void closeUserInterface() {
         actions.closeInterface();
     }
 
-    /*
-     * Prints the search results to the screen
+    /**
+     * Prints the search results to the screen.
+     * 
      * @param search Set of URLs containing a keyword
      */
     private void printSearchResults(Set<URL> search)
     {
+        // Iterate through the search results and print them.
         Iterator seIt = search.iterator();
         if(seIt.hasNext())
         {
@@ -100,9 +123,9 @@ public class Indexer implements myIWSearchEngine{
         }
     }
 
-    /*
+    /**
      * Logs text to the screen
-     * @param text text to be logged to the screen
+     * @param text text to be logged to the screen.
      */
 
     public void log(String text)
@@ -110,29 +133,32 @@ public class Indexer implements myIWSearchEngine{
         actions.log(text);
     }
 
-    /*
-     * Kills the indexer
+    /**
+     * Kills the indexer.
      */
     public void killIndexer()
     {
+        // Set indexer instance to null.
         this.indexer.setIndexerRunning(false);
         this.indexer = null;
     }
 
-    /*
-     * Stops the indexer
+    /**
+     * Stops the indexer.
      */
     public void stopIndexer()
     {
+        // Set indexerRunning state to false.
         this.indexer.setIndexerRunning(false);
     }
 
-    /*
-     * Resumes the indexer
+    /**
+     * Resumes the indexer.
      */
     public void resume() 
     {
         try {
+            // Set indexerRunning state to true.
             this.indexer.setIndexerRunning(true);
             this.indexer.resume();
         } catch (IOException ex) {
@@ -140,32 +166,60 @@ public class Indexer implements myIWSearchEngine{
         }
     }
 
+    /**
+     *
+     * @return returns the GUI status.
+     */
     public String getStatus() {
         return indexer.getStatus();
     }
 
+    /**
+     *
+     * @return returns the total URL count.
+     */
     public int totalURLCount() {
         return this.indexer.getUrlCount();
     }
 
+    /**
+     *
+     * @return returns the current URL.
+     */
     public String currentURL() {
         return this.indexer.getCurrentUrl();
     }
 
+    /**
+     *
+     * @return Returns the keyword count.
+     */
     public int KeywordsIndexedCount() {
         return this.indexer.getIndexCount();
     }
 
+    /**
+     *
+     * @return Checks if the indexer is running.
+     */
     public boolean isRunning() {
         if(this.indexer == null) return false;
         return this.indexer.getIndexerRunning();
     }
 
+    /**
+     * Checks if indexer instance is null.
+     * @return
+     */
     public boolean isNull(){
         if(this.indexer == null) return true;
         else return false;
     }
 
+    /**
+     *
+     * @return returns the keyword count.
+     */
     public int getKeywordCount() {
         return this.indexer.getKeywordCount();
     }
